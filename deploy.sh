42d3c5e84658dc834dc2c5a49667a215eb09424d
kitigai <kitigai_morita@yahoo.co.jp>
2018-04-27 10:27:05 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ set -e
 
 gcloud docker -- push us.gcr.io/${PROJECT_NAME}/spring-petclinic
 # chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment spring-petclinic -p '{"spec":{"template":{"spec":{"containers":[{"name":"spring-petclinic","image":"us.gcr.io/xxxtestxxx12345/spring-petclinic"}]}}}}'
+# kubectl patch deployment spring-petclinic -p '{"spec":{"template":{"spec":{"containers":[{"name":"spring-petclinic","image":"us.gcr.io/xxxtestxxx12345/spring-petclinic"}]}}}}'
+kubectl set image deployment/spring-petclinic spring-petclinic=us.gcr.io/xxxtestxxx12345/spring-petclinic
